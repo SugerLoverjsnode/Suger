38d3883ee6600152ad95e582a80674950a2915b9
SugerLoverjsnode <1531638633@qq.com>
2017-12-21 17:05:01 +0800

    添加了一个JS

---
+++ hello.js
@@ -0,0 +1,30 @@
+//console.log("我的第一个node应用")
+var hello = 'hello node.';
+// const str = hello.charCodeAt('e')
+//查找指定字符的unicode编码
+// const str = hello.charAt(2)
+//以指定的下标查找字符
+// const str = hello.indexOf('l')
+//查找第一个指定数值的下标，没有找到返回-1
+// const str = hello.substring(1,3)
+//返回截取的字符
+// const str = hello.lastIndexOf('l')
+//从后往前查找指定字符
+// const str = hello.substr(2,2)
+//截取字符串  第一个参数是字符的下标   第二个参数是截取个数
+// const str = hello.split('e')
+//以指定字符分割字符串
+// const str = hello.concat('aa')
+//字符串拼接
+// const str = hello.toUpperCase()
+//字符串转大写
+// const str = hello.toLowerCase()
+//字符串转小写
+// const str = hello.length
+//返回字符串的长度
+// const str = hello.search(/./i)
+//查找指定字符下标
+// const sliceNode = hello.slice(6)
+//截取字符串
+
+console.log(hello)
